1edd20c34650fe209911bcfe1cc6f0b238e497ae
CodeCompilerGitHub <80412763+CodeCompilerGitHub@users.noreply.github.com>
2021-03-20 15:23:57 -0500

    Update PasswordGen.py

--- PasswordGen.py
+++ PasswordGen.py
@@ -1,31 +1,29 @@
-import random
-import string_utils
+# Password Generater
+import string_utils # this module is for shuffling the passwords
 
-while True:
-    userChars = input("Enter the characters you want in your password: ")
-    if userChars == "":
-        break
-    userPasswords = int(input("How many passwords do you want to generate : "))
-    userLength = input("How many characters do you want in each password : ")
-    if userLength.strip() == "":
-        userLength = len(userChars)
-    if int(userLength) > len(userChars):
-        userLength = len(userChars)
-    if userLength != "" :
-        userLength = int(userLength)
-        
-    for x in range(0,userPasswords):
-        password = ""
-        password = string_utils.shuffle(userChars)
+userChars = input("Enter the characters you want in your password : ")
+userPasswords = int(input("How many passwords do you want to generate : "))
+passwordLength = input("Enter the length of your passwords : ")
 
-        passwordList = []
-        passwordList[:0] = password
-        #print(passwordList)
-        password = ""
-        e = 0
-        for i in passwordList:
-            if e >= userLength:
-                break
-            password += i
-            e += 1
-        print(password)
+if passwordLength == "" or int(passwordLength) > len(userChars):
+    passwordLength = len(userChars)
+else:
+    passwordLength = int(passwordLength)
+
+# Generate the password
+for x in range(0,userPasswords):
+    password = ""
+    password = string_utils.shuffle(userChars) # this shuffles userChar 
+
+    passwordList = []
+    passwordList[:0] = password
+    # print(passwordList) optional 
+
+    password = ""
+    number = 0
+    for i in passwordList:
+        if number >= passwordLength:
+            break
+        password += i
+        number += 1
+    print(password) # this prints the password
